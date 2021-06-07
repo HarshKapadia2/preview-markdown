069ce1745635e78853d253eb7e516cf43c5dbc9f
Harsh Kapadia <harshgkapadia@gmail.com>
2021-06-07 17:12:13 +0530

    :style: update: Add code block highlighting
    Add 'highlight.js'

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -3,10 +3,16 @@ import marked from "marked";
 import Input from "./Input";
 import Output from "./Output";
 import "../css/textarea.css";
+import "../css/highlight.css";
 import initialText from "../util/initialText";
 
 marked.setOptions({
-	xhtml: true
+	xhtml: true,
+	highlight: (code, lang) => {
+		const hljs = require("highlight.js");
+		const language = hljs.getLanguage(lang) ? lang : "plaintext";
+		return hljs.highlight(code, { language }).value;
+	}
 });
 
 const TextArea = () => {
