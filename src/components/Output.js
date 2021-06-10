c481235f8b36df5d75521445e6f0b7bf265de00b
Harsh Kapadia <harshgkapadia@gmail.com>
2021-06-10 22:39:09 +0530

    :sparkles: feat: Add dark mode to output box

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "../css/output.css";
+import moon from "../img/moon.svg";
+import sun from "../img/sun.svg";
 
 const Output = ({ innerHtml }) => {
+	const [theme, setTheme] = useState("light");
+
 	const setOutput = () => {
 		if (innerHtml) return { __html: innerHtml };
 		else return { __html: "<h2>Enter some text!</h2>" };
+	};
+
+	const changeTheme = () => {
+		if (theme === "light") setTheme("dark");
+		else setTheme("light");
 	};
 
 	return (
@@ -13,8 +23,18 @@ const Output = ({ innerHtml }) => {
 			<div
 				id="output"
 				title="Output"
+				className={theme === "light" ? "" : "dark"}
 				dangerouslySetInnerHTML={setOutput()}
 			></div>
+
+			<button
+				id="theme-btn"
+				className="action-btn"
+				title="Change theme"
+				onClick={changeTheme}
+			>
+				<img src={theme === "light" ? moon : sun} alt="Change theme" />
+			</button>
 		</div>
 	);
 };
