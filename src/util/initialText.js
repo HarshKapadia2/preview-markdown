91aacf6d4288b1687376bdd7886433aada37fba0
Harsh Kapadia <harshgkapadia@gmail.com>
2021-06-10 11:40:09 +0530

    :truck: update: Add collapsible section to `initialText.js`

--- src/util/initialText.js
+++ src/util/initialText.js
@@ -206,6 +206,15 @@ Eg:
    <img src="/preview-markdown/logo.svg" alt="Relative link to logo" width="20%" />
 </p>
 
+#### Collapsible section
+
+<details>
+   <summary>Click to toggle collapse</summary>
+
+   - Hi!
+   - Usual **Markdown** works in \`here\`.
+</details>
+
 ---
 
 <p align="center">
